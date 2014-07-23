34913b4f7036f866ada59422e8bf46f5d0ddc6f4
Robert Niederreiter <office@squarewave.at>
2014-07-23 09:58:39 +0200

    remove console logging

--- src/bdajax/resources/bdajax_bs3.js
+++ src/bdajax/resources/bdajax_bs3.js
@@ -12,18 +12,20 @@
     $(document).ready(function() {
         $.tools.overlay.conf.top = 0;
         $.tools.overlay.conf.fixed = true;
+
         $(document).bind('bdajax_overlay_before_load', function(event) {
             event.elem.css('overflow-y', 'scroll');
             $('body', document)
                 .css('padding-right', '13px')
                 .addClass('modal-open');
         });
+
         $(document).bind('bdajax_overlay_load', function(event) {
             event.elem.css('position', 'fixed');
             event.elem.css('top', '0');
         });
+
         $(document).bind('bdajax_overlay_close', function(event) {
-            console.log($('.modal:visible').length);
             if (!$('.modal:visible').length) {
                 $('body', document)
                     .css('padding-right', '')

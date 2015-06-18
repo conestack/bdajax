81a495876d0ae7b7f225b97709eca26cf102810c
Johannes Raggam <raggam-nl@adm.at>
2015-06-18 15:04:54 +0200

    Revert "fix dependencies"
    This reverts commit 3ef2763409b254cdc2aea199bb4e3d587eb4690b.

--- src/bdajax/resources/bdajax.js
+++ src/bdajax/resources/bdajax.js
@@ -1,5 +1,5 @@
 /* jslint browser: true */
-/* global jQuery */
+/* global jQuery, bdajax */
 /*
  * bdajax v1.6.0
  *
@@ -11,10 +11,44 @@
  * - jQuery Tools overlay.js
  */
 
-var bdajax;
-
-(function($) {
+(function($, bdajax) {
     "use strict";
+
+    $(document).ready(function() {
+        bdajax.spinner.hide();
+        $(document).bdajax();
+    });
+
+    $.fn.bdajax = function() {
+        var context = $(this);
+        $('*', context).each(function() {
+            for (var i in this.attributes) {
+                var attr = this.attributes[i];
+                if (attr && attr.nodeName) {
+                    var name = attr.nodeName;
+                    if (name.indexOf('ajax:bind') > -1) {
+                        var events = attr.nodeValue;
+                        var ajax = $(this);
+                        ajax.unbind(events);
+                        if (ajax.attr('ajax:action') || ajax.attr(
+                            'ajax:event') || ajax.attr(
+                            'ajax:overlay')) {
+                            ajax.bind(events, bdajax._dispatching_handler);
+                        }
+                    }
+                    if (name.indexOf('ajax:form') > -1) {
+                        bdajax.prepare_ajax_form($(this));
+                    }
+                }
+            }
+        });
+        // B/C: Ajax forms have a dedicated ``ajax:form`` directive now.
+        bdajax.bind_ajax_form(context);
+        for (var binder in bdajax.binders) {
+            bdajax.binders[binder](context);
+        }
+        return context;
+    };
 
     bdajax = {
 
@@ -567,42 +601,4 @@ var bdajax;
         }
     };
 
-
-    $.fn.bdajax = function() {
-        var context = $(this);
-        $('*', context).each(function() {
-            for (var i in this.attributes) {
-                var attr = this.attributes[i];
-                if (attr && attr.nodeName) {
-                    var name = attr.nodeName;
-                    if (name.indexOf('ajax:bind') > -1) {
-                        var events = attr.nodeValue;
-                        var ajax = $(this);
-                        ajax.unbind(events);
-                        if (ajax.attr('ajax:action') || ajax.attr(
-                            'ajax:event') || ajax.attr(
-                            'ajax:overlay')) {
-                            ajax.bind(events, bdajax._dispatching_handler);
-                        }
-                    }
-                    if (name.indexOf('ajax:form') > -1) {
-                        bdajax.prepare_ajax_form($(this));
-                    }
-                }
-            }
-        });
-        // B/C: Ajax forms have a dedicated ``ajax:form`` directive now.
-        bdajax.bind_ajax_form(context);
-        for (var binder in bdajax.binders) {
-            bdajax.binders[binder](context);
-        }
-        return context;
-    };
-
-
-    $(document).ready(function() {
-        bdajax.spinner.hide();
-        $(document).bdajax();
-    });
-
-}(jQuery));
+}(jQuery, bdajax));

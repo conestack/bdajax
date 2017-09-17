cfe580494bac30e4fe302b90683c028bc6017aee
Robert Niederreiter <office@squarewave.at>
2017-09-17 11:56:25 +0200

    Prepare 1.10

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-1.10 (unreleased)
+1.10 (2017-09-17)
 -----------------
 
 - Hidden form response iframe gets created on the fly and removed after form
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read_file(name):
         return f.read()
 
 
-version = '1.10.dev0'
+version = '1.10'
 shortdesc = 'Ajax convenience.'
 longdesc = '\n\n'.join([read_file(name) for name in [
     'README.rst',
